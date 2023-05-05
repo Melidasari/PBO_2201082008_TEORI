3302ef89a3b97b2569a35dd60ee5892d70a5a4d2
Melidasari <127370941+Melidasari@users.noreply.github.com>
2023-05-05 12:22:29 +0700

    Upload Latihan Modul 8

---
+++ src/Melida280423/Latihan8_soal1.java
@@ -0,0 +1,18 @@
+package Melida280423;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Latihan8_soal1 {
+     public static void main ( String[] args){
+        for(int i=0; i<=4; i++)
+        System.out.println("\n" +args[i]);
+         
+    }
+}
